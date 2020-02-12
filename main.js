00ceb33544ce31090fab714e3e958029fc8d1543
tmac9494 <trentmcdole94@gmail.com>
2020-02-12 14:47:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -17,10 +17,14 @@ const getPosition = function ( elem ) {
   return location >= 0 ? location : 0;
 };
 
-const isElementInView = (element, buffer=0) => {
-
+const isElementInView = (element) => {
+    const transforms = new WebKitCSSMatrix(
+      window.getComputedStyle(element).webkitTransform
+    );
+    // const buffer = transforms.m42 < 0 ? transforms.m42 * -1 : transforms.m42;
+    const buffer = transforms.m42 < 0 ? transforms.m42 : 0;
     const yPos = getPosition(element);
 		const topOfWindow = window.scrollY;
 		const bottomOfWindow = window.scrollY + window.innerHeight;
-		return bottomOfWindow > yPos - buffer && topOfWindow < yPos + element.clientHeight;
+		return bottomOfWindow > yPos + buffer && topOfWindow < yPos + element.clientHeight;
 }
